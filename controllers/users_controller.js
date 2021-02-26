fad797fa658f059b4215ac320781ea1a47e3fad3
kandari012 <73176498+kandari012@users.noreply.github.com>
2021-02-26 23:18:18 +0530

    manual authentication without passpost js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,25 +1,48 @@
 const User = require("../models/user");
 
 module.exports.profile = function (req, res) {
-  return res.render("users_profile", {
-    title: "Profile",
-  });
+  if (req.cookies.user_id) {
+    User.findById(req.cookies.user_id, function (err, user) {
+      if (err) {
+        console.log("error while finding the user");
+        return;
+      }
+
+      if (user) {
+        return res.render("users_profile", {
+          title: "User-Profile",
+          user: user,
+        });
+      } else {
+        return res.redirect("/users/sign-in");
+      }
+    });
+  } else {
+    return res.redirect("/users/sign-in");
+  }
 };
 
 module.exports.posts = function (req, res) {
   return res.end("<h1> UserPosts </h1>");
 };
 
-module.exports.signUP = function (req, res) {
+module.exports.signUp = function (req, res) {
   return res.render("user_sign_Up", { title: "signUP" });
 };
 
 module.exports.signIn = function (req, res) {
   return res.render("user_sign_In", { title: "signIn" });
 };
+
+module.exports.signOut = function (req, res) {
+  res.clearCookie("user_id");
+  return res.redirect("/users/sign-in");
+};
+
 //to create new users
 
 module.exports.create = function (req, res) {
+  //if pasword and confirm password not match
   if (req.body.password != req.body.confirm_password) {
     return res.redirect("back");
   }
@@ -38,6 +61,29 @@ module.exports.create = function (req, res) {
         return res.redirect("/users/sign-in");
       });
     } else {
+      return res.redirect("back");
+    }
+  });
+};
+
+module.exports.createSession = function (req, res) {
+  //find the user
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("error while finding the user");
+      return;
+    }
+    //handle user found
+    if (user) {
+      //handle password which not match
+      if (user.password != req.body.password) {
+        return res.redirect("back");
+      }
+      //handle session creation
+      res.cookie("user_id", user.id);
+      return res.redirect("/users/profile");
+    } else {
+      //handle user not found
       return res.redirect("back");
     }
   });
--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,9 @@ console.log("routes exported");
 
 router.get("/profile", userController.profile);
 router.get("/posts", userController.posts);
-router.get("/sign-up", userController.signUP);
+router.get("/sign-up", userController.signUp);
 router.get("/sign-in", userController.signIn);
 router.post("/create", userController.create);
+router.post("/create-session", userController.createSession);
+router.get("/sign-out", userController.signOut);
 module.exports = router;
