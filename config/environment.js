c3177785ecf911e5b7adee65a5abe7e4f1967413
rahul1 <rahulkandari012@gmail.com>
2021-10-22 14:16:19 +0530

    production environment

--- config/environment.js
+++ config/environment.js
@@ -24,6 +24,33 @@ const development = {
 
 const production = {
   name: "production",
+  asset_path: process.env.asset_path,
+  session_cookie_key: process.env.session_cookie_key,
+  db: process.env.db,
+  smtp: {
+    //for setting gmail
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      //id with which you will be sending those mails
+      user: process.env.user,
+      pass: process.env.pass,
+    },
+  },
+  google_client_id: process.env.google_client_id,
+  google_client_secret: process.env.google_client_secret,
+  google_callback_url: process.env.google_callback_url,
+  jwt_secret: process.env.jwt_secret,
 };
 
-module.exports = development;
+console.log(
+  "enviromment variable ----------------",
+  eval(process.env.NODE_ENV)
+);
+
+module.exports =
+  eval(process.env.NODE_ENV) == undefined
+    ? development
+    : eval(process.env.NODE_ENV);
--- index.js
+++ index.js
@@ -107,5 +107,5 @@ app.listen(port, function (err) {
   if (err) {
     console.log(`error in running the server :  ${port}`);
   }
-  console.log(`server is running on port :  ${port}`);
+  console.log(`server is running on port :  ${port}`, process.env);
 });
